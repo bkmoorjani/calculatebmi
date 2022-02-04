c55535dd69681d650fb08d8a0e19b906672adb07
bkmoorjani <87546071+bkmoorjani@users.noreply.github.com>
2022-02-04 07:48:52 -0600

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+ 
+import './App.css';
+import UserData from './components/UserData';
+
+
+const  App=(props)=> {
+  return (
+    <div >
+      
+      <UserData></UserData>
+   
+    </div>
+  );
+}
+
+export default App;
